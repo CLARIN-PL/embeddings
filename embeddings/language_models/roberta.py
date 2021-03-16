55e48590fdbb8c7626bd8e4cd7851de71b25b386
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2021-03-16 19:10:04 +0100

    use our v1 polish roberta from hugging face repo

--- embeddings/language_models/roberta.py
+++ embeddings/language_models/roberta.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("clarin-polish-roberta-tokenizer")
-model = AutoModel.from_pretrained("clarin-polish-roberta-tokenizer")
+tokenizer = AutoTokenizer.from_pretrained("clarin/roberta-polish-v1")
+model = AutoModel.from_pretrained("clarin/roberta-polish-v1")
 
 encoded_input = tokenizer.encode("Zażółć gęślą jaźń", return_tensors="pt")
 outputs = model(encoded_input)
