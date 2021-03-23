d0834affe75ed0ad72e5c9f8c12e7ad15b8ba34e
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2021-03-23 11:27:52 +0100

    mains

--- experimental/embeddings/language_models/roberta.py
+++ experimental/embeddings/language_models/roberta.py
@@ -1,7 +1,13 @@
 from transformers import AutoTokenizer, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("clarin-pl/roberta-polish-kgr10")
-model = AutoModel.from_pretrained("clarin-pl/roberta-polish-kgr10")
 
-encoded_input = tokenizer.encode("Zażółć gęślą jaźń", return_tensors="pt")
-outputs = model(encoded_input)
+def main() -> None:
+    tokenizer = AutoTokenizer.from_pretrained("clarin-pl/roberta-polish-kgr10")
+    model = AutoModel.from_pretrained("clarin-pl/roberta-polish-kgr10")
+
+    encoded_input = tokenizer.encode("Zażółć gęślą jaźń", return_tensors="pt")
+    outputs = model(encoded_input)
+
+
+if __name__ == "__main__":
+    main()
