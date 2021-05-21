5326a0f6829e24edba3ebb404b284630d781ebaa
ktagowski <kamil.tagowski@gmail.com>
2021-05-21 11:13:52 +0200

    Add text classification pipeline test

---
+++ tests/test_text_classification.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+from pprint import pprint
+
+import typer
+from flair.embeddings import TransformerDocumentEmbeddings
+import pytest
+from embeddings.data.hugging_face_data_loader import HuggingFaceDataLoader
+from embeddings.data.hugging_face_dataset import HuggingFaceDataset
+from embeddings.embedding.flair_embedding import FlairTransformerDocumentEmbedding
+from embeddings.evaluator.text_classification_evaluator import TextClassificationEvaluator
+from embeddings.model.flair_model import FlairModel
+from embeddings.pipeline.standard_pipeline import StandardPipeline
+from embeddings.task.flair_task import FlairTextClassification
+from embeddings.transformation.flair_transformations import ToFlairCorpusTransformation
+from experimental.defaults import RESULTS_PATH
+import numpy as np
+
+
+@pytest.fixture
+def text_classification_pipeline() -> StandardPipeline:
+    dataset = HuggingFaceDataset(
+        "clarin-pl/polemo2-official",
+        train_domains=["reviews"],
+        dev_domains=["reviews"],
+        test_domains=["reviews"],
+        text_cfg="sentence",
+    )
+    data_loader = HuggingFaceDataLoader()
+    transformation = ToFlairCorpusTransformation("text", "target")
+    embedding = FlairTransformerDocumentEmbedding("allegro/herbert-base-cased")
+    task = FlairTextClassification(RESULTS_PATH, task_train_kwargs={"max_epochs": 1})
+    model = FlairModel(embedding, task)
+    evaluator = TextClassificationEvaluator()
+    pipeline = StandardPipeline(dataset, data_loader, transformation, model, evaluator)
+    return pipeline
+
+
+def test_text_classification_pipeline(text_classification_pipeline: StandardPipeline) -> None:
+    result = text_classification_pipeline.run()
+    assert 0 < result[0]["accuracy"] < 1
