594cf85f9b13a0bd935ff6e0b1fbc6a921f7f7c1
albert.sawczyn <albertsawczyn@gmail.com>
2021-05-22 15:45:25 +0200

    refactor(pipeline): add legacy script, update sequence tagging evaluation script

--- experimental/embeddings/scripts/evaluate_sequence_tagging.py
+++ experimental/embeddings/scripts/evaluate_sequence_tagging.py
@@ -1,64 +1,45 @@
-import pickle
 from pathlib import Path
-from typing import Optional
+from pprint import pprint
 
-import experimental.datasets.utils.misc
-import flair
-import torch
 import typer
-from experimental.defaults import RESULTS_PATH
-from experimental.embeddings.tasks import sequence_tagging as st
-from flair.embeddings import TransformerWordEmbeddings
 
+from embeddings.data.hugging_face_data_loader import HuggingFaceDataLoader
+from embeddings.data.hugging_face_dataset import HuggingFaceDataset
+from embeddings.embedding.flair_embedding import FlairTransformerWordEmbedding
+from embeddings.evaluator.sequence_tagging_evaluator import SequenceTaggingEvaluator
+from embeddings.model.flair_model import FlairModel
+from embeddings.pipeline.standard_pipeline import StandardPipeline
+from embeddings.task.flair.sequence_tagging import SequenceTagging
+from embeddings.transformation.flair.column_corpus_transformation import ColumnCorpusTransformation
+from experimental.defaults import RESULTS_PATH
 
 app = typer.Typer()
 
 
-def setup(device: str) -> None:
-    flair.device = torch.device(device)  # type:ignore
-    flair.set_seed(441)
-
-
-def evaluate_sequence_tagging(
-    embedding: str = typer.Option(..., help="Huggingface embedding model name"),
-    dataset_name: str = typer.Option(..., help="Dataset name"),
-    dataset_url: Optional[str] = typer.Option(None, help="Dataset download url"),
-    root: str = typer.Option(
-        RESULTS_PATH.joinpath("tagging"),
-        help="Root directory for output files",
-    ),
-    device: str = typer.Option("cpu", help="Device for Flair e.g. cpu"),
+def run(
+    embedding_name: str = typer.Option(..., help="Hugging Face embedding model name or path."),
+    dataset_name: str = typer.Option(..., help="Hugging Face dataset name."),
+    input_column_name: str = typer.Option(...),
+    target_column_name: str = typer.Option(...),
+    root: str = typer.Option(RESULTS_PATH.joinpath("document_classification")),
+    hidden_size: int = typer.Option(..., help="Number of hidden states in RNN."),
 ) -> None:
-    setup(device)
+    pprint(locals())
 
-    out_dir = Path(root, embedding, dataset_name)
-    out_dir.mkdir(parents=True, exist_ok=True)
+    output_path = Path(root, embedding_name, dataset_name)
+    output_path.mkdir(parents=True, exist_ok=True)
 
-    ds_args = {"url": dataset_url} if dataset_url else {}
-    dataset_cls = experimental.datasets.utils.misc.get_dataset_cls(dataset_name)
-    dataset = dataset_cls(**ds_args)
-    corpus = dataset.to_flair_column_corpus()
-    tag_dictionary = corpus.make_tag_dictionary(tag_type="tag")
+    dataset = HuggingFaceDataset(dataset_name)
+    data_loader = HuggingFaceDataLoader()
+    transformation = ColumnCorpusTransformation(input_column_name, target_column_name)
+    embedding = FlairTransformerWordEmbedding(embedding_name)
+    task = SequenceTagging(output_path, hidden_size=256)
+    model = FlairModel(embedding, task)
+    evaluator = SequenceTaggingEvaluator()
 
-    test_sentences = corpus.test.sentences.copy()
-
-    print("Loading embeddings...")
-    embeddings = TransformerWordEmbeddings(embedding)
-
-    print("Training model...")
-    tagger = st.FlairSequenceTagger(
-        embeddings=embeddings,
-        hidden_dim=256,
-        tag_dictionary=tag_dictionary,
-        output_path=str(out_dir),
-    )
-
-    training_log = tagger.fit(corpus)
-    evaluation_log = tagger.evaluate(test_sentences)
-    with out_dir.joinpath("results.pkl").open(mode="wb") as f:
-        pickle.dump(obj={"training_log": training_log, "evaluation_log": evaluation_log}, file=f)
-
-    print("Done!", training_log)
+    pipeline = StandardPipeline(dataset, data_loader, transformation, model, evaluator)
+    result = pipeline.run()
+    typer.echo(result)
 
 
-typer.run(evaluate_sequence_tagging)
+typer.run(run)
