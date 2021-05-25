18b49b673601637f0e36fda32bed56c39cfe76e6
Kamil Tagowski <kamil.tagowski@gmail.com>
2021-05-25 22:49:09 +0200

    Add pipeline tests (#16)
    * Add text classification pipeline test
    
    * Add container for transformations (#17)
    
    * Add container for transformations
    
    * Add corpus downsampling
    
    * Fix mypy errors
    
    * Change metric and fix assert
    
    * Adapt test to pipeline changes
    
    * Fix styling
    
    * Add sequence tagging test
    
    * Fix file encoding
    
    * Change paths to temporal
    
    * creating type safe transformation logic (#22)
    
    * Setup random state
    
    Co-authored-by: Roman Bartusiak <riomus@gmail.com>

--- embeddings/transformation/transformation.py
+++ embeddings/transformation/transformation.py
@@ -1,11 +1,12 @@
 import abc
-from typing import TypeVar, Generic
+from typing import TypeVar, Generic, List, Any
 from abc import ABC
-
 from embeddings.utils.loggers import get_logger
 
 Input = TypeVar("Input")
 Output = TypeVar("Output")
+NewOutput = TypeVar("NewOutput")
+OutputInternal = TypeVar("OutputInternal")
 
 
 class Transformation(ABC, Generic[Input, Output]):
@@ -18,3 +19,24 @@ class Transformation(ABC, Generic[Input, Output]):
     @abc.abstractmethod
     def transform(self, data: Input) -> Output:
         pass
+
+    def then(
+        self, right: "Transformation[Output, NewOutput]"
+    ) -> "Transformation[Input, NewOutput]":
+        return CombainedTransformations(self, right)
+
+
+class CombainedTransformations(
+    Transformation[Input, Output], Generic[Input, Output, OutputInternal]
+):
+    def __init__(
+        self,
+        left: Transformation[Input, OutputInternal],
+        right: Transformation[OutputInternal, Output],
+    ) -> None:
+        self.left = left
+        self.right = right
+
+    def transform(self, data: Input) -> Output:
+        intermidiate = self.left.transform(data)
+        return self.right.transform(intermidiate)
---
+++ tests/test_sequence_tagging.py
@@ -0,0 +1,54 @@
+from tempfile import TemporaryDirectory
+from typing import Dict, List, Any, Tuple
+
+import datasets
+import flair
+import numpy as np
+import pytest
+from embeddings.data.hugging_face_data_loader import HuggingFaceDataLoader
+from embeddings.data.hugging_face_dataset import HuggingFaceDataset
+from embeddings.embedding.flair_embedding import FlairTransformerWordEmbedding
+from embeddings.evaluator.sequence_tagging_evaluator import SequenceTaggingEvaluator
+from embeddings.model.flair_model import FlairModel
+from embeddings.pipeline.standard_pipeline import StandardPipeline
+from embeddings.task.flair.sequence_tagging import SequenceTagging
+from embeddings.transformation.flair.column_corpus_transformation import ColumnCorpusTransformation
+from embeddings.transformation.flair.corpus_transformations import (
+    DownsampleFlairCorpusTransformation,
+)
+from flair.data import Corpus
+
+
+@pytest.fixture  # type: ignore
+def sequence_tagging_pipeline(
+    result_path: "TemporaryDirectory[str]",
+) -> Tuple[
+    StandardPipeline[str, datasets.DatasetDict, Corpus, Dict[str, np.ndarray], List[Any]],
+    "TemporaryDirectory[str]",
+]:
+    dataset = HuggingFaceDataset("clarin-pl/nkjp-pos")
+    data_loader = HuggingFaceDataLoader()
+    transformation = ColumnCorpusTransformation("tokens", "pos_tags").then(
+        DownsampleFlairCorpusTransformation(percentage=0.001)
+    )
+    embedding = FlairTransformerWordEmbedding("allegro/herbert-base-cased")
+    task = SequenceTagging(result_path.name, hidden_size=256, task_train_kwargs={"max_epochs": 1})
+    model = FlairModel(embedding, task)
+    evaluator = SequenceTaggingEvaluator()
+
+    pipeline = StandardPipeline(dataset, data_loader, transformation, model, evaluator)
+    return pipeline, result_path
+
+
+def test_sequence_tagging_pipeline(
+    sequence_tagging_pipeline: Tuple[
+        StandardPipeline[str, datasets.DatasetDict, Corpus, Dict[str, np.ndarray], List[Any]],
+        "TemporaryDirectory[str]",
+    ],
+) -> None:
+    flair.set_seed(441)
+    pipeline, path = sequence_tagging_pipeline
+    result = pipeline.run()
+    path.cleanup()
+
+    np.testing.assert_almost_equal(result[0]["overall_f1"], 0.023668639053254437)
