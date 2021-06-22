a421bb3c562319cfd20fecb5ee8193d0b4365695
Kamil Tagowski <kamil.tagowski@gmail.com>
2021-06-22 19:50:26 +0200

    Add pos tagging metrics calculation fix (#46)

--- embeddings/evaluator/sequence_tagging_evaluator.py
+++ embeddings/evaluator/sequence_tagging_evaluator.py
@@ -1,13 +1,22 @@
 from typing import Any, Dict, List, Tuple
 
-from datasets import Metric, load_metric
+import datasets
 
 from embeddings.evaluator.metrics_evaluator import MetricsEvaluator
+from embeddings.metric.pos_metric import POSTaggingSeqevalMetric
 
 
 class SequenceTaggingEvaluator(MetricsEvaluator):
     @property
-    def metrics(self) -> List[Tuple[Metric, Dict[str, Any]]]:
+    def metrics(self) -> List[Tuple[datasets.Metric, Dict[str, Any]]]:
         return [
-            (load_metric("seqeval"), {}),
+            (datasets.load_metric("seqeval"), {}),
         ]
+
+
+class POSTaggingEvaluator(MetricsEvaluator):
+    @property
+    def metrics(
+        self,
+    ) -> List[Tuple[POSTaggingSeqevalMetric, Dict[str, Any]]]:
+        return [(POSTaggingSeqevalMetric(), {})]
--- tests/test_pos_tagging.py
+++ tests/test_pos_tagging.py
@@ -10,7 +10,7 @@ from flair.data import Corpus
 from embeddings.data.hugging_face_data_loader import HuggingFaceDataLoader
 from embeddings.data.hugging_face_dataset import HuggingFaceDataset
 from embeddings.embedding.flair_embedding import FlairTransformerWordEmbedding
-from embeddings.evaluator.sequence_tagging_evaluator import SequenceTaggingEvaluator
+from embeddings.evaluator.sequence_tagging_evaluator import POSTaggingEvaluator
 from embeddings.model.flair_model import FlairModel
 from embeddings.pipeline.standard_pipeline import StandardPipeline
 from embeddings.task.flair_task.sequence_tagging import SequenceTagging
@@ -37,7 +37,7 @@ def sequence_tagging_pipeline(
     embedding = FlairTransformerWordEmbedding("allegro/herbert-base-cased")
     task = SequenceTagging(result_path.name, hidden_size=256, task_train_kwargs={"max_epochs": 1})
     model = FlairModel(embedding, task)
-    evaluator = SequenceTaggingEvaluator()
+    evaluator = POSTaggingEvaluator()
 
     pipeline = StandardPipeline(dataset, data_loader, transformation, model, evaluator)
     return pipeline, result_path
@@ -54,4 +54,6 @@ def test_sequence_tagging_pipeline(
     result = pipeline.run()
     path.cleanup()
 
-    np.testing.assert_almost_equal(result["seqeval"]["overall_f1"], 0.023668639053254437)
+    np.testing.assert_almost_equal(
+        result["POSTaggingSeqevalMetric"]["overall_f1"], 0.14788732394366197
+    )
