a3e763d8bffea8166251accf364a2f8c4c7444a5
ktagowski <kamil.tagowski@gmail.com>
2021-06-22 15:11:05 +0200

    Add pos tagging metrics calculation fix

---
+++ embeddings/metric/pos_metric.py
@@ -0,0 +1,41 @@
+from itertools import chain
+from typing import Any, Dict, List
+
+import numpy as np
+from datasets import load_metric
+
+from embeddings.metric import metric
+
+
+class POSTaggingSeqevalMetric(metric.Metric[np.ndarray, Dict[str, Any]]):
+    ALLOWED_TAG_PREFIXES = {"U-"}
+
+    def __init__(self) -> None:
+        name = type(self).__name__
+        super().__init__(name)
+
+    @property
+    def _seqeval_args(self) -> Dict[str, Any]:
+        return {"suffix": None, "scheme": "BILOU", "mode": "strict"}
+
+    @staticmethod
+    def _convert_single_tag_to_bilou_scheme(tags: np.ndarray) -> List[List[str]]:
+        return [[f"U-{tag}" for tag in sequence] for sequence in tags]
+
+    @staticmethod
+    def _have_tags_unit_prefix(tags: np.ndarray) -> bool:
+        unique_tags = set(chain.from_iterable(tags))
+        tag_prefixes = set(it[0:2] for it in unique_tags)
+        return len(tag_prefixes.difference(POSTaggingSeqevalMetric.ALLOWED_TAG_PREFIXES)) < 1
+
+    def compute(self, y_true: np.ndarray, y_pred: np.ndarray, **kwargs: Any) -> Dict[str, Any]:
+        seqeval = load_metric("seqeval")
+
+        if not self._have_tags_unit_prefix(y_pred):
+            y_pred = self._convert_single_tag_to_bilou_scheme(y_pred)
+        if not self._have_tags_unit_prefix(y_true):
+            y_true = self._convert_single_tag_to_bilou_scheme(y_true)
+
+        outs = seqeval.compute(references=y_true, predictions=y_pred, **self._seqeval_args)
+        assert isinstance(outs, Dict)
+        return outs
