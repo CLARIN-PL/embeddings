6890bee2652046d8442065ca16de94ac35251f0d
Kamil Tagowski <kamil.tagowski@gmail.com>
2021-07-06 15:14:11 +0200

    Add support for ner tagging (#49)
    * Add support for NER tagging

--- examples/evaluate_sequence_labelling.py
+++ examples/evaluate_sequence_labelling.py
@@ -1,10 +1,11 @@
 import pprint
 from pathlib import Path
+from typing import Optional
 
 import typer
 
 from embeddings.defaults import RESULTS_PATH
-from embeddings.pipeline.hugging_face_POS_tagging import HuggingFacePOSTaggingPipeline
+from embeddings.pipeline.hugging_face_sequence_labeling import HuggingFaceSequenceLabelingPipeline
 
 app = typer.Typer()
 
@@ -14,29 +15,37 @@ def run(
         "allegro/herbert-base-cased", help="Hugging Face embedding model name or path."
     ),
     dataset_name: str = typer.Option(
-        "clarin-pl/nkjp-pos", help="Hugging Face dataset name or path."
+        "clarin-pl/kpwr-ner", help="Hugging Face dataset name or path."
     ),
     input_column_name: str = typer.Option(
         "tokens", help="Column name that contains text to classify."
     ),
     target_column_name: str = typer.Option(
-        "pos_tags", help="Column name that contains tag labels for POS tagging."
+        "ner", help="Column name that contains tag labels for POS tagging."
     ),
     root: str = typer.Option(RESULTS_PATH.joinpath("pos_tagging")),
     hidden_size: int = typer.Option(256, help="Number of hidden states in RNN."),
+    evaluation_mode: str = typer.Option(
+        "conll", help="Evaluation mode. Supported modes: [unit, conll, strict]."
+    ),
+    tagging_scheme: Optional[str] = typer.Option(
+        None, help="Tagging scheme. Supported schemes: [IOB1, IOB2, IOE1, IOE2, IOBES, BILOU]"
+    ),
 ) -> None:
     typer.echo(pprint.pformat(locals()))
 
     output_path = Path(root, embedding_name, dataset_name)
     output_path.mkdir(parents=True, exist_ok=True)
 
-    pipeline = HuggingFacePOSTaggingPipeline(
+    pipeline = HuggingFaceSequenceLabelingPipeline(
         embedding_name,
         dataset_name,
         input_column_name,
         target_column_name,
         output_path,
         hidden_size,
+        evaluation_mode,
+        tagging_scheme,
     )
     result = pipeline.run()
     typer.echo(pprint.pformat(result))
--- tests/test_sequence_labelling_evaluator.py
+++ tests/test_sequence_labelling_evaluator.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 from sklearn.metrics import classification_report, precision_recall_fscore_support
 
-from embeddings.evaluator.sequence_labeling_evaluator import POSTaggingEvaluator
+from embeddings.evaluator.sequence_labeling_evaluator import SequenceLabelingEvaluator
 
 
 @pytest.fixture  # type: ignore
@@ -24,6 +24,26 @@ def data() -> Dict[str, np.ndarray]:
                 ["RB", "VB", "VB"],
                 ["NN", "NN", "VB", "NN"],
                 ["JJ", "RB", "RB", "VB"],
+            ],
+            dtype=object,
+        ),
+    }
+
+
+@pytest.fixture  # type: ignore
+def ner_data() -> Dict[str, np.ndarray]:
+    return {
+        "y_true": np.array(
+            [
+                ["B-RB", "I-RB", "O"],
+                ["B-KT", "O", "B-KT", "O"],
+            ],
+            dtype=object,
+        ),
+        "y_pred": np.array(
+            [
+                ["I-RB", "I-RB", "O"],
+                ["B-KT", "O", "I-KT", "O"],
             ],
             dtype=object,
         ),
@@ -70,8 +90,8 @@ def sklearn_metrics(data: Dict[str, np.ndarray]) -> Dict[str, Union[Dict[str, fl
 
 @pytest.fixture  # type: ignore
 def seqeval_metrics(data: Dict[str, np.ndarray]) -> Dict[str, Union[Dict[str, float], float]]:
-    evaluator = POSTaggingEvaluator()
-    out = evaluator.evaluate(data)["POSTaggingSeqeval"]
+    evaluator = SequenceLabelingEvaluator(evaluation_mode="unit")
+    out = evaluator.evaluate(data)["UnitSeqeval"]
     assert isinstance(out, dict)
     return out
 
@@ -84,3 +104,15 @@ def test_pos_tagging_metrics(
         sklearn_metrics[metric] == pytest.approx(seqeval_metrics[metric])
         for metric in sklearn_metrics.keys()
     )
+
+
+def test_conll_metrics(ner_data: Dict[str, np.ndarray]) -> None:
+    evaluator = SequenceLabelingEvaluator(evaluation_mode="conll")
+    out = evaluator.evaluate(ner_data)
+    np.testing.assert_almost_equal(out["seqeval__mode_None__scheme_None"]["overall_f1"], 1.0)
+
+
+def test_strict_metrics(ner_data: Dict[str, np.ndarray]) -> None:
+    evaluator = SequenceLabelingEvaluator(evaluation_mode="strict", tagging_scheme="IOB2")
+    out = evaluator.evaluate(ner_data)
+    np.testing.assert_almost_equal(out["seqeval__mode_strict__scheme_IOB2"]["overall_f1"], 0.5)
