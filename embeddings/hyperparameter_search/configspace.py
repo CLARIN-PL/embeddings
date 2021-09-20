fb1944ed1275ca64868f7d6a5555fdcd2a94eed4
ktagowski <kamil.tagowski@gmail.com>
2021-09-20 20:42:36 +0200

    Add configspace and parameters wrappers

---
+++ embeddings/hyperparameter_search/configspace.py
@@ -0,0 +1,110 @@
+import abc
+from dataclasses import dataclass, field
+from typing import Any, Dict, Tuple, Union
+
+import optuna
+
+from embeddings.hyperparameter_search.parameters import ConstantParameter, SearchableParameter
+
+Parameter = Union[SearchableParameter, ConstantParameter]
+
+
+class AbstractConfigSpace(abc.ABC):
+    def _parse_parameter(
+        self, param_name: str, trial: optuna.trial.Trial
+    ) -> Tuple[str, Union[None, float, int, str, bool]]:
+        param: Parameter = self.__getattribute__(param_name)
+        if isinstance(param, SearchableParameter):
+            return param.name, trial._suggest(name=param.name, distribution=param.distribution)
+        elif isinstance(param, ConstantParameter):
+            return param.name, param.value
+        else:
+            raise ValueError(
+                f"Parameter type {type(param)} is not suported! "
+                "Supported types are: SearchableParameter and ConstantParameter"
+            )
+
+    def _map_parameters(
+        self, trial: optuna.trial.Trial, parameters_types: Dict[str, Any]
+    ) -> Dict[str, Union[None, float, int, str, bool]]:
+        parameters: Dict[str, Union[None, float, int, str, bool]] = {}
+        for param_name, param_type in parameters_types.items():
+            parameters.update([self._parse_parameter(trial=trial, param_name=param_name)])
+
+        return parameters
+
+    @abc.abstractmethod
+    def sample_parameters(
+        self, trial: optuna.trial.Trial
+    ) -> Dict[str, Union[None, float, int, str, bool]]:
+        pass
+
+
+@dataclass
+class SequenceLabelingConfigSpace(AbstractConfigSpace):
+    hidden_size: Parameter = SearchableParameter(
+        name="hidden_size", type="int_uniform", low=128, high=2048, step=128
+    )
+    use_rnn: Parameter = SearchableParameter(
+        name="use_rnn", type="categorical", choices=[True, False]
+    )
+    rnn_type: Parameter = SearchableParameter(
+        name="rnn_type", type="categorical", choices=["LSTM", "GRU"]
+    )
+    rnn_layers: Parameter = SearchableParameter(
+        name="rnn_layers", type="int_uniform", low=1, high=3, step=1
+    )
+    dropout: Parameter = SearchableParameter(
+        name="dropout", type="discrete_uniform", low=0.0, high=0.5, q=0.05
+    )
+    word_dropout: Parameter = SearchableParameter(
+        name="word_dropout", type="discrete_uniform", low=0.0, high=0.5, q=0.05
+    )
+    locked_dropout: Parameter = SearchableParameter(
+        name="locked_dropout", type="discrete_uniform", low=0.0, high=0.5, q=0.05
+    )
+    reproject_embeddings: Parameter = SearchableParameter(
+        name="reproject_embeddings", type="categorical", choices=[True, False]
+    )
+    use_crf: Parameter = SearchableParameter(
+        name="use_crf", type="categorical", choices=[True, False]
+    )
+    learning_rate: Parameter = SearchableParameter(
+        name="learning_rate", type="log_uniform", low=1e-4, high=1e-1
+    )
+    mini_batch_size: Parameter = SearchableParameter(
+        name="mini_batch_size", type="log_int_uniform", low=16, high=256, step=1
+    )
+    max_epochs: Parameter = SearchableParameter(
+        name="max_epochs", type="int_uniform", low=1, high=5, step=1
+    )
+    param_selection_mode: Parameter = field(
+        init=False, default=ConstantParameter(name="param_selection_mode", value=True)
+    )
+    save_final_model: Parameter = field(
+        init=False, default=ConstantParameter(name="save_final_model", value=False)
+    )
+
+    def sample_parameters(
+        self, trial: optuna.trial.Trial
+    ) -> Dict[str, Union[None, float, int, str, bool]]:
+        parameters = {}
+
+        use_rnn_name, use_rnn_val = self._parse_parameter(trial=trial, param_name="use_rnn")
+        parameters[use_rnn_name] = use_rnn_val
+
+        if use_rnn_val:
+            for rnn_param in ("rnn_layers", "rnn_type"):
+                parameters.update([self._parse_parameter(trial=trial, param_name=rnn_param)])
+
+        parameters.update(
+            self._map_parameters(
+                parameters_types={
+                    param_name: param_cfg
+                    for param_name, param_cfg in self.__annotations__.items()
+                    if param_name not in {"rnn_layers", "rnn_type", "use_rnn"}
+                },
+                trial=trial,
+            )
+        )
+        return parameters
---
+++ embeddings/hyperparameter_search/parameters.py
@@ -0,0 +1,170 @@
+import abc
+from abc import ABC
+from dataclasses import dataclass, field
+from typing import Generic, Literal, Optional, Sequence, TypeVar, Union, get_args
+
+import optuna
+import optuna.distributions
+
+Distribution = TypeVar("Distribution")
+ChoiceType = Union[None, bool, int, float, str]
+Numeric = Union[float, int]
+ParameterTypes = Literal[
+    "categorical",
+    "uniform",
+    "log_uniform",
+    "discrete_uniform",
+    "int_uniform",
+    "log_int_uniform",
+]
+
+
+@dataclass(frozen=True)
+class ConstantParameter:
+    name: str
+    value: ChoiceType
+
+
+# Type: ignore is due to https://github.com/python/mypy/issues/5374
+@dataclass  # type:ignore
+class AbstractSearchableParameter(ABC, Generic[Distribution]):
+    name: str
+    type: ParameterTypes
+    distribution: Distribution = field(init=False)
+    low: Optional[Numeric] = None
+    high: Optional[Numeric] = None
+    step: Optional[Numeric] = None
+    q: Optional[float] = None
+    choices: Optional[Sequence[ChoiceType]] = None
+
+    @staticmethod
+    def _check_additional_params_passed(
+        variables: Sequence[Union[ChoiceType, Sequence[ChoiceType]]]
+    ) -> None:
+        for var in variables:
+            assert var is None
+
+    def __post_init__(self) -> None:
+        if self.type == "categorical":
+            assert self.choices is not None
+            AbstractSearchableParameter._check_additional_params_passed(
+                (self.low, self.high, self.step, self.q)
+            )
+
+            self.distribution = self.get_categorical_distribution(choices=self.choices)
+        elif self.type == "uniform":
+            assert isinstance(self.low, float)
+            assert isinstance(self.high, float)
+            AbstractSearchableParameter._check_additional_params_passed(
+                (self.choices, self.step, self.q)
+            )
+
+            self.distribution = self.get_uniform_distribution(low=self.low, high=self.high)
+        elif self.type == "log_uniform":
+            assert isinstance(self.low, float)
+            assert isinstance(self.high, float)
+            AbstractSearchableParameter._check_additional_params_passed(
+                (self.choices, self.step, self.q)
+            )
+
+            self.distribution = self.get_log_uniform_distribution(
+                low=self.low,
+                high=self.high,
+            )
+        elif self.type == "discrete_uniform":
+            assert isinstance(self.low, float)
+            assert isinstance(self.high, float)
+            assert isinstance(self.q, float)
+            AbstractSearchableParameter._check_additional_params_passed((self.choices, self.step))
+
+            self.distribution = self.get_discrete_uniform_distribution(
+                low=self.low, high=self.high, q=self.q
+            )
+
+        elif self.type == "int_uniform":
+            assert isinstance(self.low, int)
+            assert isinstance(self.high, int)
+            assert isinstance(self.step, int)
+            AbstractSearchableParameter._check_additional_params_passed((self.choices, self.q))
+
+            self.distribution = self.get_int_uniform_distribution(
+                low=self.low, high=self.high, step=self.step
+            )
+        elif self.type == "log_int_uniform":
+            assert isinstance(self.low, int)
+            assert isinstance(self.high, int)
+            assert isinstance(self.step, int)
+            AbstractSearchableParameter._check_additional_params_passed((self.choices, self.q))
+            self.distribution = self.get_log_int_uniform_distribution(
+                low=self.low, high=self.high, step=self.step
+            )
+        else:
+            raise ValueError(
+                f"Parameter {self.type} is not supported. Pick one of {get_args(ParameterTypes)}"
+            )
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_categorical_distribution(choices: Sequence[ChoiceType]) -> Distribution:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_uniform_distribution(low: float, high: float) -> Distribution:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_log_uniform_distribution(low: float, high: float) -> Distribution:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_discrete_uniform_distribution(low: float, high: float, q: float) -> Distribution:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_int_uniform_distribution(low: int, high: int, step: int) -> Distribution:
+        pass
+
+    @staticmethod
+    @abc.abstractmethod
+    def get_log_int_uniform_distribution(low: int, high: int, step: int) -> Distribution:
+        pass
+
+
+class SearchableParameter(AbstractSearchableParameter[optuna.distributions.BaseDistribution]):
+    @staticmethod
+    def get_categorical_distribution(
+        choices: Sequence[ChoiceType],
+    ) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.CategoricalDistribution(choices=choices)
+
+    @staticmethod
+    def get_uniform_distribution(low: float, high: float) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.UniformDistribution(low=low, high=high)
+
+    @staticmethod
+    def get_log_uniform_distribution(
+        low: float, high: float
+    ) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.LogUniformDistribution(low=low, high=high)
+
+    @staticmethod
+    def get_discrete_uniform_distribution(
+        low: float, high: float, q: float
+    ) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.DiscreteUniformDistribution(low=low, high=high, q=q)
+
+    @staticmethod
+    def get_int_uniform_distribution(
+        low: int, high: int, step: int
+    ) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.IntUniformDistribution(low=low, high=high, step=step)
+
+    @staticmethod
+    def get_log_int_uniform_distribution(
+        low: int, high: int, step: int
+    ) -> optuna.distributions.BaseDistribution:
+        return optuna.distributions.IntLogUniformDistribution(low=low, high=high, step=step)
