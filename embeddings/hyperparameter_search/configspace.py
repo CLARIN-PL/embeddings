a20feeb6250f492a220703d5e234cb5b9843a1ae
ktagowski <kamil.tagowski@gmail.com>
2021-09-26 22:36:50 +0200

    Update ConfigSpace

--- embeddings/hyperparameter_search/configspace.py
+++ embeddings/hyperparameter_search/configspace.py
@@ -1,6 +1,7 @@
 import abc
+from abc import ABC
 from dataclasses import dataclass, field
-from typing import Any, Dict, Final, Tuple, Union
+from typing import Any, Dict, Final, Generic, Tuple, Type, TypeVar, Union
 
 import optuna
 
@@ -8,9 +9,10 @@ from embeddings.hyperparameter_search.parameters import ConstantParameter, Searc
 from embeddings.utils.utils import PrimitiveTypes
 
 Parameter = Union[SearchableParameter, ConstantParameter]
+ParsedParameters = TypeVar("ParsedParameters")
 
 
-class AbstractConfigSpace(abc.ABC):
+class AbstractConfigSpace(ABC, Generic[ParsedParameters]):
     def _parse_parameter(
         self, param_name: str, trial: optuna.trial.Trial
     ) -> Tuple[str, PrimitiveTypes]:
@@ -34,13 +36,80 @@ class AbstractConfigSpace(abc.ABC):
 
         return parameters
 
-    @abc.abstractmethod
+    @classmethod
+    def _get_annotations(cls) -> Dict[str, Type[Parameter]]:
+        annotations = {}
+        for c in cls.mro():
+            annotations.update(getattr(c, "__annotations__", {}))
+        return annotations
+
     def sample_parameters(self, trial: optuna.trial.Trial) -> Dict[str, PrimitiveTypes]:
+        return self._map_parameters(
+            parameters_types={
+                param_name: param_cfg for param_name, param_cfg in self._get_annotations().items()
+            },
+            trial=trial,
+        )
+
+    @staticmethod
+    @abc.abstractmethod
+    def parse_parameters(parameters: Dict[str, PrimitiveTypes]) -> ParsedParameters:
         pass
 
 
+@dataclass  # type: ignore
+class AbstractFlairModelTrainerConfigSpace(AbstractConfigSpace[ParsedParameters], ABC):
+    param_selection_mode: Parameter = field(
+        init=False, default=ConstantParameter(name="param_selection_mode", value=True)
+    )
+    save_final_model: Parameter = field(
+        init=False, default=ConstantParameter(name="save_final_model", value=False)
+    )
+    learning_rate: Parameter = SearchableParameter(
+        name="learning_rate", type="log_uniform", low=1e-4, high=1e-1
+    )
+    mini_batch_size: Parameter = SearchableParameter(
+        name="mini_batch_size", type="log_int_uniform", low=16, high=256, step=1
+    )
+    max_epochs: Parameter = SearchableParameter(
+        name="max_epochs", type="int_uniform", low=1, high=5, step=1
+    )
+
+    @staticmethod
+    def _parse_model_trainer_parameters(
+        parameters: Dict[str, PrimitiveTypes]
+    ) -> Tuple[Dict[str, PrimitiveTypes], Dict[str, PrimitiveTypes]]:
+        task_train_keys: Final = {
+            "learning_rate",
+            "mini_batch_size",
+            "max_epochs",
+            "param_selection_mode",
+            "save_final_model",
+        }
+        task_train_kwargs = {k: parameters.pop(k) for k in task_train_keys if k in parameters}
+        return parameters, task_train_kwargs
+
+
+class FlairModelTrainerConfigSpace(AbstractFlairModelTrainerConfigSpace[Dict[str, PrimitiveTypes]]):
+    @staticmethod
+    def parse_parameters(parameters: Dict[str, PrimitiveTypes]) -> Dict[str, PrimitiveTypes]:
+        (
+            parameters,
+            task_train_kwargs,
+        ) = FlairModelTrainerConfigSpace._parse_model_trainer_parameters(parameters)
+        if len(parameters):
+            raise ValueError(
+                f"Some of the parameters are not mapped. Unmapped parameters: {parameters}"
+            )
+        return task_train_kwargs
+
+
 @dataclass
-class SequenceLabelingConfigSpace(AbstractConfigSpace):
+class SequenceLabelingConfigSpace(
+    AbstractFlairModelTrainerConfigSpace[
+        Tuple[int, Dict[str, PrimitiveTypes], Dict[str, PrimitiveTypes]]
+    ]
+):
     hidden_size: Parameter = SearchableParameter(
         name="hidden_size", type="int_uniform", low=128, high=2048, step=128
     )
@@ -68,21 +137,6 @@ class SequenceLabelingConfigSpace(AbstractConfigSpace):
     use_crf: Parameter = SearchableParameter(
         name="use_crf", type="categorical", choices=[True, False]
     )
-    learning_rate: Parameter = SearchableParameter(
-        name="learning_rate", type="log_uniform", low=1e-4, high=1e-1
-    )
-    mini_batch_size: Parameter = SearchableParameter(
-        name="mini_batch_size", type="log_int_uniform", low=16, high=256, step=1
-    )
-    max_epochs: Parameter = SearchableParameter(
-        name="max_epochs", type="int_uniform", low=1, high=5, step=1
-    )
-    param_selection_mode: Parameter = field(
-        init=False, default=ConstantParameter(name="param_selection_mode", value=True)
-    )
-    save_final_model: Parameter = field(
-        init=False, default=ConstantParameter(name="save_final_model", value=False)
-    )
 
     def sample_parameters(self, trial: optuna.trial.Trial) -> Dict[str, PrimitiveTypes]:
         parameters = {}
@@ -98,7 +152,7 @@ class SequenceLabelingConfigSpace(AbstractConfigSpace):
             self._map_parameters(
                 parameters_types={
                     param_name: param_cfg
-                    for param_name, param_cfg in self.__annotations__.items()
+                    for param_name, param_cfg in self._get_annotations().items()
                     if param_name not in {"rnn_layers", "rnn_type", "use_rnn"}
                 },
                 trial=trial,
@@ -122,16 +176,11 @@ class SequenceLabelingConfigSpace(AbstractConfigSpace):
             "rnn_layers",
             "rnn_type",
         }
-        task_train_keys: Final = {
-            "learning_rate",
-            "mini_batch_size",
-            "max_epochs",
-            "param_selection_mode",
-            "save_final_model",
-        }
 
         task_model_kwargs = {k: parameters.pop(k) for k in task_model_keys if k in parameters}
-        task_train_kwargs = {k: parameters.pop(k) for k in task_train_keys if k in parameters}
+        parameters, task_train_kwargs = SequenceLabelingConfigSpace._parse_model_trainer_parameters(
+            parameters=parameters
+        )
 
         if len(parameters):
             raise ValueError(
