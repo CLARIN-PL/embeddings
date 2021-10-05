353f70d11d6af537018f55827f55251cbaeb4d58
djaniak <denis.janiak7@gmail.com>
2021-10-05 14:56:43 +0200

    refactor: refactor code

--- embeddings/transformation/flair_transformation/drop_subset_corpus_transformation.py
+++ embeddings/transformation/flair_transformation/drop_subset_corpus_transformation.py
@@ -7,7 +7,10 @@ class DropSubsetFlairCorpusTransformation(Transformation[Corpus, Corpus]):
     FLAIR_SUBSETS = ["dev", "test"]
 
     def __init__(self, subset: str):
-        assert subset in self.FLAIR_SUBSETS
+        if subset not in self.FLAIR_SUBSETS:
+            raise ValueError(
+                "Wrong subset name given in the argument - you can drop only dev or test subset."
+            )
         self.subset = subset
 
     def transform(self, data: Corpus) -> Corpus:
