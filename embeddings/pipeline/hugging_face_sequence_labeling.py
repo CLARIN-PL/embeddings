cc6a468d306ed72be0aa836efa3dbe919dce9356
ktagowski <kamil.tagowski@gmail.com>
2021-11-02 22:00:35 +0100

    Fix load_dataset_kwargs

--- embeddings/pipeline/hugging_face_sequence_labeling.py
+++ embeddings/pipeline/hugging_face_sequence_labeling.py
@@ -43,7 +43,7 @@ class HuggingFaceSequenceLabelingPipeline(
         task_train_kwargs: Optional[Dict[str, Any]] = None,
         load_dataset_kwargs: Optional[Dict[str, Any]] = None,
     ):
-        dataset = HuggingFaceDataset(dataset=dataset_name, load_dataset_kwargs=load_dataset_kwargs)
+        dataset = HuggingFaceDataset(dataset_name, **load_dataset_kwargs)
         data_loader = HuggingFaceDataLoader()
         transformation: Union[
             Transformation[datasets.DatasetDict, Corpus], Transformation[Corpus, Corpus]
