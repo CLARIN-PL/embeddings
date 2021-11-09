bc7b7d3c1766167e2dcaaedc17b03cf7b09ebdf8
djaniak <denis.janiak7@gmail.com>
2021-11-09 13:51:09 +0100

    fix: implement text classification pipeline with proper datamodules

---
+++ embeddings/embedding/document_embedding.py
@@ -0,0 +1,18 @@
+from typing import Any, List, Literal
+
+import torch
+
+
+class DocumentPoolEmbedding:
+    SUPPORTED_STRATEGIES: List[str] = ["mean", "cls"]
+
+    def __init__(self, strategy: Literal["mean", "cls"]):
+        if strategy not in self.SUPPORTED_STRATEGIES:
+            raise ValueError("Wrong strategy given as the argument")
+        self.strategy = strategy
+
+    def __call__(self, model_output: torch.Tensor, *args: Any, **kwargs: Any) -> torch.Tensor:
+        if self.strategy == "mean":
+            return model_output.mean(dim=1)
+        elif self.strategy == "cls":
+            return model_output[:, 0]
