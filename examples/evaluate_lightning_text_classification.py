c4c83238c9e97dafc535ddc3660dbc5c2e44de11
djaniak <denis.janiak7@gmail.com>
2021-12-09 13:45:26 +0100

    refactor: fix example code

--- examples/evaluate_lightning_text_classification.py
+++ examples/evaluate_lightning_text_classification.py
@@ -1,42 +1,36 @@
 import pprint
 from pathlib import Path
 
+from pytorch_lightning.callbacks import EarlyStopping
+
 from embeddings.defaults import RESULTS_PATH
 from embeddings.pipeline.lightning_classification import LightningClassificationPipeline
 
 root: Path = RESULTS_PATH.joinpath("lightning_sequence_classification")
 root.mkdir(parents=True, exist_ok=True)
 
+
 pipeline = LightningClassificationPipeline(
     embedding_name="allegro/herbert-base-cased",
-    dataset_name="clarin-pl/cst-wikinews",
-    input_column_name=["sentence_1", "sentence_2"],
+    dataset_name="clarin-pl/polemo2-official",
+    input_column_name=["text"],
+    target_column_name="target",
     output_path=root,
-    target_column_name="label",
-    task_train_kwargs={
-        "max_epochs": 1,
-        "gpus": 1,
-        "limit_val_batches": 0,
-        "num_sanity_val_steps": 0,
+    load_dataset_kwargs={
+        "train_domains": ["hotels", "medicine"],
+        "dev_domains": ["hotels", "medicine"],
+        "test_domains": ["hotels", "medicine"],
+        "text_cfg": "text",
     },
-    task_model_kwargs={"pool_strategy": "cls", "learning_rate": 5e-4},
+    task_train_kwargs={
+        "max_epochs": 10,
+        "gpus": 1,
+        "callbacks": [
+            EarlyStopping(monitor="val/F1", verbose=True, patience=5, min_delta=0.01, mode="max")
+        ],
+    },
+    task_model_kwargs={"learning_rate": 5e-4},
 )
-
-# pipeline = TextClassificationPipeline(
-#     embedding_name="allegro/herbert-base-cased",
-#     dataset_name="clarin-pl/polemo2-official",
-#     input_column_name=["text"],
-#     target_column_name="target",
-#     output_path=root,
-#     load_dataset_kwargs={
-#         "train_domains": ["hotels", "medicine"],
-#         "dev_domains": ["hotels", "medicine"],
-#         "test_domains": ["hotels", "medicine"],
-#         "text_cfg": "text",
-#     },
-#     task_train_kwargs={"max_epochs": 2, "gpus": 1},
-#     task_model_kwargs={"learning_rate": 5e-4, "use_scheduler": True},
-# )
 
 result = pipeline.run()
 pprint.pformat(result)
