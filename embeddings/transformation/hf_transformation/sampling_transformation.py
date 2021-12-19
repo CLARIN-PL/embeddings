da3f6870f51aea09840089112f14009077954815
ktagowski <kamil.tagowski@gmail.com>
2021-12-20 00:22:43 +0100

    feat(transformation): Update sampling transformation

--- embeddings/transformation/hf_transformation/sampling_transformation.py
+++ embeddings/transformation/hf_transformation/sampling_transformation.py
@@ -31,7 +31,12 @@ class SampleSplitsHuggingFaceTransformation(
     def transform(self, data: datasets.DatasetDict) -> datasets.DatasetDict:
         dataset = datasets.DatasetDict()
 
-        if self.dev_fraction and self.test_fraction:
+        if (
+            self.dev_fraction
+            and self.test_fraction
+            and "validation" not in data.keys()
+            and "test" not in data.keys()
+        ):
             dev_test_fraction = self.dev_fraction + self.test_fraction
             held_out_dataset = self._train_test_split(data["train"], dev_test_fraction)
             dataset["train"] = held_out_dataset["train"]
@@ -41,12 +46,14 @@ class SampleSplitsHuggingFaceTransformation(
             dataset["validation"] = test_valid_dataset["train"]
             dataset["test"] = test_valid_dataset["test"]
 
-        elif self.dev_fraction:
+        elif self.dev_fraction and "validation" not in data:
             sampled_dataset = self._train_test_split(data["train"], self.dev_fraction)
             dataset["train"] = sampled_dataset["train"]
             dataset["validation"] = sampled_dataset["test"]
 
-        elif self.test_fraction:
+        elif self.test_fraction and "test" not in data:
             dataset = self._train_test_split(data["train"], self.test_fraction)
 
+        if not dataset:
+            dataset = data
         return dataset
