8efcc83ecca020442423112f854e7afe2e929109
Kamil Tagowski <kamil.tagowski@pwr.edu.pl>
2021-12-23 03:01:54 +0100

    tests: Add HuggingFaceTextClassificationPreprocessingPipeline test

--- tests/test_hf_preprocessing_pipeline.py
+++ tests/test_hf_preprocessing_pipeline.py
@@ -4,8 +4,11 @@ from copy import deepcopy
 import datasets
 import pytest
 
-from embeddings.data.data_loader import HuggingFaceDataLoader
+from embeddings.data.data_loader import HuggingFaceDataLoader, HuggingFaceLocalDataLoader
 from embeddings.data.dataset import HuggingFaceDataset
+from embeddings.pipeline.hf_preprocessing_pipeline import (
+    HuggingFaceTextClassificationPreprocessingPipeline,
+)
 from embeddings.transformation.hf_transformation.drop_subset_transformation import (
     DropSubsetHuggingFaceCorpusTransformation,
 )
@@ -18,6 +21,18 @@ from embeddings.transformation.hf_transformation.sampling_transformation import 
 def hf_dataset_cfg():
     return {
         "dataset": "clarin-pl/polemo2-official",
+    }
+
+
+@pytest.fixture
+def hf_preprocessing_pipeline_kwargs(hf_dataset_cfg, result_path):
+    return {
+        "dataset_name": hf_dataset_cfg["dataset"],
+        "persist_path": result_path.name,
+        "sample_missing_splits": None,
+        "ignore_test_subset": True,
+        "seed": 441,
+        "load_dataset_kwargs": None,
     }
 
 
@@ -52,3 +67,16 @@ def test_sample_splits_huggingface_transformation_with_non_existing_keys(hf_data
     assert math.floor(len(hf_dataset["train"]) * 0.7) == len(transfomed_ds["train"])
     assert math.floor(len(hf_dataset["train"]) * 0.2) == len(transfomed_ds["validation"])
     assert math.ceil(len(hf_dataset["train"]) * 0.1) == len(transfomed_ds["test"])
+
+
+def test_huggingface_text_classification_preprocessing_pipeline(hf_preprocessing_pipeline_kwargs):
+    pipeline = HuggingFaceTextClassificationPreprocessingPipeline(
+        **hf_preprocessing_pipeline_kwargs
+    )
+    pipeline.run()
+
+    dataset = HuggingFaceLocalDataLoader().load(
+        HuggingFaceDataset(hf_preprocessing_pipeline_kwargs["persist_path"])
+    )
+    assert isinstance(dataset, datasets.DatasetDict)
+    assert frozenset(dataset.keys()) == frozenset(("train", "validation"))
