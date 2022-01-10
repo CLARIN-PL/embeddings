3e4a921f61606cb22494cdef063f8963d8f8f949
Mariusz Kossakowski <44444134+mkossakowski19@users.noreply.github.com>
2022-01-10 12:50:07 +0100

    Initial sklearn code

---
+++ embeddings/pipeline/sklearn_classification.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict, Optional, Sequence, Union
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from xgboost.sklearn import XGBClassifier
+
+
