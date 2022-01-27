76c8a12a4f6231fc48ab56fc51b1c61670b7051a
Lukasz Koziol <code.woke@gmail.com>
2022-01-27 07:48:17 +0100

    Move WordEmbeddingsPL class to experimental directory

--- embeddings/embedding/static/embedding.py
+++ embeddings/embedding/static/embedding.py
@@ -5,7 +5,7 @@ from flair.embeddings import WordEmbeddings
 
 from embeddings.embedding.flair_embedding import FlairDocumentPoolEmbedding, FlairEmbedding
 from embeddings.embedding.static.config import SingleFileConfig, StaticModelHubConfig
-from embeddings.embedding.static.flair import WordEmbeddingsPL
+from experimental.embeddings.static.flair import WordEmbeddingsPL
 from embeddings.utils.utils import import_from_string
 
 
