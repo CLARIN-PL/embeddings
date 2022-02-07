aa3d60840629c8252e6934f42d7487140c2df0b9
Mariusz Kossakowski <44444134+mkossakowski19@users.noreply.github.com>
2022-02-07 16:35:41 +0100

    Add tests for transformations

---
+++ tests/test_rename_input_columns_transformation.py
@@ -0,0 +1,49 @@
+import pytest
+
+from embeddings.data.data_loader import HuggingFaceDataLoader
+from embeddings.data.dataset import HuggingFaceDataset
+from embeddings.transformation.hf_transformation.to_pandas_transformation import (
+    ToPandasHuggingFaceCorpusTransformation,
+)
+from embeddings.transformation.pandas_transformation.rename_input_columns_transformation import (
+    RenameInputColumnsTransformation,
+)
+
+
+@pytest.fixture(scope="module")
+def hf_dataset(dataset_name: str = "clarin-pl/polemo2-official"):
+    return HuggingFaceDataset(dataset_name)
+
+
+@pytest.fixture(scope="module")
+def data_loader():
+    return HuggingFaceDataLoader()
+
+
+@pytest.fixture(scope="module")
+def rename_columns_transformation(
+    input_column_name: str = "text", target_column_name: str = "target"
+):
+    return ToPandasHuggingFaceCorpusTransformation().then(
+        RenameInputColumnsTransformation(input_column_name, target_column_name)
+    )
+
+
+def test_to_pandas_transformation(
+    hf_dataset: HuggingFaceDataset,
+    data_loader: HuggingFaceDataLoader,
+    rename_columns_transformation: ToPandasHuggingFaceCorpusTransformation,
+):
+    dataset = hf_dataset
+    loader = data_loader
+    transformation = rename_columns_transformation
+
+    data = loader.load(dataset)
+    transformed_data = transformation.transform(data)
+
+    assert "x" in transformed_data["train"].columns
+    assert "x" in transformed_data["validation"].columns
+    assert "x" in transformed_data["test"].columns
+    assert "y" in transformed_data["train"].columns
+    assert "y" in transformed_data["validation"].columns
+    assert "y" in transformed_data["test"].columns
