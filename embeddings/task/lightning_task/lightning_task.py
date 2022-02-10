fcca5d7dcab40ab796f141c5d91ee3fc6b00751c
Lukasz R <lukasz.rucz@gmail.com>
2022-02-10 16:09:52 +0100

    Add validation flag for best epoch callback

--- embeddings/task/lightning_task/lightning_task.py
+++ embeddings/task/lightning_task/lightning_task.py
@@ -59,10 +59,14 @@ class LightningTask(Task[HuggingFaceDataModule, Dict[str, nptyping.NDArray[Any]]
     ) -> None:
         if not self.model:
             raise self.MODEL_UNDEFINED_EXCEPTION
+
+        callbacks = []
+        if "validation" in data:
+            callbacks.append(BestEpochCallback())
+            callbacks.append(EarlyStopping(**self.early_stopping_kwargs))
+
         self.trainer = pl.Trainer(
-            default_root_dir=str(self.output_path),
-            callbacks=[BestEpochCallback(), EarlyStopping(**self.early_stopping_kwargs)],
-            **self.task_train_kwargs
+            default_root_dir=str(self.output_path), callbacks=callbacks, **self.task_train_kwargs
         )
         try:
             self.trainer.fit(self.model, data)
