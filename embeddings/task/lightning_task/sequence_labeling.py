140d0b5b4df68ae420f1ee788675c834f04966de
Lukasz R <lukasz.rucz@gmail.com>
2022-02-15 11:05:59 +0100

    Add early stopping (#202)
    * Add early stopping
    
    * Add best epoch callback
    
    * Refactor
    
    * Chagen value of patiance parameter
    
    * Add test for best epoch callback
    
    * Black formatting
    
    * Add types to Callable
    
    * Add early stopping
    
    * Add best epoch callback
    
    * Refactor
    
    * Chagen value of patiance parameter
    
    * Add test for best epoch callback
    
    * Black formatting
    
    * Add types to Callable
    
    * Add validation flag for best epoch callback
    
    * Small fix
    
    * Fix for missing metric
    
    * validation hack
    
    * Black fixes
    
    * Flake fixes
    
    * Remove main
    
    Co-authored-by: Kamil Tagowski <kamil.tagowski@pwr.edu.pl>

--- embeddings/task/lightning_task/sequence_labeling.py
+++ embeddings/task/lightning_task/sequence_labeling.py
@@ -17,11 +17,12 @@ class SequenceLabelingTask(LightningTask):
         model_config_kwargs: Dict[str, Any],
         task_model_kwargs: Dict[str, Any],
         task_train_kwargs: Dict[str, Any],
+        early_stopping_kwargs: Dict[str, Any],
         train_batch_size: int = 32,
         eval_batch_size: int = 32,
         finetune_last_n_layers: int = -1,
     ) -> None:
-        super().__init__(output_path, task_train_kwargs)
+        super().__init__(output_path, task_train_kwargs, early_stopping_kwargs)
         self.model_name_or_path = model_name_or_path
         self.model_config_kwargs = model_config_kwargs
         self.task_model_kwargs = task_model_kwargs
