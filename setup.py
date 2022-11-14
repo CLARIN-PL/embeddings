20251280a68de44a075bed683240ae333157a8fe
Łukasz Augustyniak <lukasz.augustyniak@pwr.edu.pl>
2022-11-14 13:50:30 +0100

    cleanup and black

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from pkg_resources import parse_version
 from configparser import ConfigParser
+
 import setuptools
+from pkg_resources import parse_version
 
 assert parse_version(setuptools.__version__) >= parse_version("36.2")
 
